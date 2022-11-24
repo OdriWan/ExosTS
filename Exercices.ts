cb8d7d29e6a6b3de71e9102c4921e588e8701ecb
Audrey <audrey.grout@gmail.com>
2022-11-24 11:24:21 +0100

    Exercice 2

--- Exercices.ts
+++ Exercices.ts
@@ -1,8 +1,18 @@
 // Exercice 1 -> Fonction qui affiche bonjour suivi du prénom
 
-/** Fonction qui retourne "Bonjour" suivi du prénom passée en paramètre*/
+/** Retourne "Bonjour" suivi du prénom passée en paramètre*/
 const hello = function(name: string) {
     return console.log(`Bonjour ${name}`);
 }
 
 hello("Audrey");
+
+
+// Exercice 2 -> Fonction qui prend deux nombres en paramètres et retourne la somme de ces deux nombres
+
+//** Retourne la somme de deux nombres passés en paramètre */
+const somme = function(nombre1: number, nombre2: number) {
+    return console.log(`${nombre1} + ${nombre2} = ${nombre1 + nombre2}`);
+}
+
+somme(18, 9);
