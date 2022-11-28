d2f905bc8f833318c08f58eee753a310b41362a8
Audrey <audrey.grout@gmail.com>
2022-11-28 11:54:44 +0100

    Error01.ts

---
+++ Error01.ts
@@ -0,0 +1,13 @@
+class Formateur {
+  nom!: string;
+  prenom!: string;
+}
+
+let formateur1: Formateur = new Formateur();
+
+formateur1.nom = "Devos";
+formateur1.prenom = "Alex";
+
+console.log(
+  `${formateur1.nom} ${formateur1.prenom} est le formateur aujourd'hui`
+);
