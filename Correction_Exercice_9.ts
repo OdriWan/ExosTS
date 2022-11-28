82c222d6baa6d5106b803b3c00275bb90a719cc8
Audrey <audrey.grout@gmail.com>
2022-11-28 10:31:00 +0100

    Correction_Exercice_9.ts

---
+++ Correction_Exercice_9.ts
@@ -0,0 +1,19 @@
+function monnaieDeux(nombre: number): void {
+  const monnaies: number[] = [
+    500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.02, 0.01,
+  ];
+
+  for (let monnaie of monnaies) {
+    let nombreMonnaie = Math.floor(nombre / monnaie);
+    if (nombreMonnaie !== 0) {
+      if (monnaie >= 5) {
+        console.log(`Billet(s) de ${monnaie}€ : ${nombreMonnaie}`);
+      } else {
+        console.log(`Pièce(s) de ${monnaie}€ : ${nombreMonnaie}`);
+      }
+      nombre -= nombreMonnaie * monnaie;
+    }
+  }
+}
+
+monnaieDeux(888.88);
